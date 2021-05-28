41bc785780a42047e9c7707b3ce7b520df209cbc
linoaulaspucpr <84945223+linoaulaspucpr@users.noreply.github.com>
2021-05-28 00:17:10 -0300

    Update descriptografar.php

--- php/descriptografar.php
+++ php/descriptografar.php
@@ -5,7 +5,7 @@
 
 	$dados = $_POST["dados"];
 
-	$chave_privada = file_get_contents("../chave/chave_privada.pem");
+	$chave_privada = file_get_contents("../chave/chave_privada.pem")
 
 	openssl_private_decrypt(base64_decode($dados), $mensagem_descriptografada, $chave_privada, OPENSSL_ZERO_PADDING);
 
